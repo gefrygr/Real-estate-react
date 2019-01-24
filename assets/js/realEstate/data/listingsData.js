293e84553f73d5932f6356580a81447b969e1427
Gefry Grullon <gefrygrullon@gmail.com>
2019-01-24 11:00:10 -0500

    Initial commit

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,105 @@
+var listingsData = [
+    {
+        address: 'West Palm Beach',
+        city: 'Miami',
+        state: 'FL',
+        rooms: 2,
+        price: 135000,
+        floorSpace: 200,
+        extras: [
+            'elevator',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4011-1/moda-apartments-exterior.jpg'
+    },
+    {
+        address: 'Westchase',
+        city: 'Tampa',
+        state: 'FL',
+        rooms: 3,
+        price: 350000,
+        floorSpace: 3500,
+        extras: [
+            'elevator',
+            'Swimming Pool',
+            'gym'
+        ],
+        homeType: 'Condo',
+        image: 'https://images1.apartments.com/i2/zuaUa-FjipQvMwZND26ZKldKCclk0vOMoGGfsXcbmSU/111/eleanor-apartments-seattle-seattle-wa-primary-photo.jpg'
+    },
+    {
+        address: 'Riverview',
+        city: 'Wesley Chappels',
+        state: 'FL',
+        rooms: 3,
+        price: 230000,
+        floorSpace: 2000,
+        extras: [
+            'elevator',
+            'Swimming Pool',
+            'gym'
+        ],
+        homeType: 'Single Home',
+        image: 'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_1200,w_2000/v1515110114/g5/g5-c-5csdjnplb-millburn-company/g5-cl-1hnmyetc6k-the-hills-at-renaissance/uploads/_MG_1156_dw7sou.jpg'
+    },
+    {
+        address: 'Oakland Park',
+        city: 'Fort Lauderdale',
+        state: 'FL',
+        rooms: 1,
+        price: 165000,
+        floorSpace: 500,
+        extras: [
+            'Swimming Pool',
+            'gym'
+        ],
+        homeType: 'Apartment',
+        image: 'https://media.bizj.us/view/img/11148146/the-meadows-at-kildaire-email*750xx1000-563-0-52.jpg'
+    },
+    {
+        address: 'Timberlane',
+        city: 'Tampa',
+        state: 'FL',
+        rooms: 0,
+        price: 105000,
+        floorSpace: 300,
+        extras: [
+            'Internet',
+            'gym'
+        ],
+        homeType: 'Studio',
+        image: 'http://www.ishtar.it/wp/wp-content/uploads/2017/10/blueSpiritStudio_04.jpg'
+    },
+    {
+        address: 'Ridgewoods',
+        city: 'New York City',
+        state: 'New York',
+        rooms: 3,
+        price: 450000,
+        floorSpace: 2100,
+        extras: [
+            'Pool',
+            'Community'
+        ],
+        homeType: 'House',
+        image: 'https://cdn.houseplans.com/product/q5qkhirat4bcjrr4rpg9fk3q94/w800x533.jpg?v=8'
+    },
+    {
+        address: 'Dunedin',
+        city: 'Clearwater',
+        state: 'FL',
+        rooms: 2,
+        price: 300000,
+        floorSpace: 1000,
+        extras: [
+            'elevator',
+            'Swimming Pool',
+            'gym',
+        ],
+        homeType: 'Multi Home',
+        image: 'https://pmcvariety.files.wordpress.com/2018/07/bradybunchhouse_sc11.jpg?w=1000&h=563&crop=1'
+    }
+]
+
+export default listingsData;
